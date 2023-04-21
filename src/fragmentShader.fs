21da9a44effcb8db7bfa0a0e1334df0059a4cb23
shenhua <shenhua@ihuman.com>
2023-04-21 15:21:15 +0800

    light shader;

--- src/fragmentShader.fs
+++ src/fragmentShader.fs
@@ -1,11 +1,46 @@
 #version 330 core
+in vec3 FragPos;
+in vec3 Normal;
 out vec4 FragColor;
 
-in vec2 TexCoord;
+uniform vec3 objectColor;
+uniform vec3 lightColor;
+uniform vec3 lightPos;
+uniform vec3 viewPos;
 
-uniform sampler2D texture1;
+struct Material{
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+    float shininess;
+};
+
+uniform Material material;
+
+struct Light {
+    vec3 position;
+
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+};
+
+uniform Light light;
 
 void main()
-{   
-    FragColor = texture(texture1, vec2(TexCoord.x, TexCoord.y));
+{
+    vec3 ambient = material.ambient * light.ambient;
+
+    vec3 norm = normalize(Normal);
+    vec3 lightDir = normalize(lightPos - FragPos);
+    float diff = max(dot(norm, lightDir), 0.0);
+    vec3 diffuse = (diff * material.diffuse) * light.diffuse;
+
+    vec3 viewDir = normalize(viewPos - FragPos);
+    vec3 reflectDir = reflect(-lightDir, norm);
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+    vec3 specular = (spec * material.specular) * light.specular;
+
+    vec3 result = ambient + diffuse + specular;
+    FragColor = vec4(result, 1.0);
 }
